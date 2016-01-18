727277cba7615e2a8de4d5b4062e8b25a0ad49c3
michaellennox <michaelctlennox@gmail.com>
2016-01-18 16:00:17 +0000

    Restaurant validates uniqueness of name, review validates inclusion in range 0..5

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
   it { is_expected.to validate_length_of(:name).is_at_least(3) }
-  # it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
 end
