22f6e979512120b701c90485476ab3673f5fde5a
michaellennox <michaelctlennox@gmail.com>
2016-01-20 17:06:57 +0000

    Adds failing feature test for ensuring user cannot review the same restaurant multiple times

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -13,4 +13,20 @@ feature 'reviews' do
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
+
+  scenario 'a user should not be able to review multiple times' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: 'so so'
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: 'epicsauce'
+    select '5', from: 'Rating'
+    click_button 'Leave Review'
+
+    expect(page).not_to have_content 'epicsauce'
+    expect(page).to have_content 'You cannot review the same restaurant more than once'
+  end
 end
