52e1bdb3a5c2e2af19a3f345c7a5c17c7279b987
michaellennox <michaelctlennox@gmail.com>
2016-01-21 15:31:02 +0000

    Puts star rating in the right place

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -2,4 +2,9 @@ module RestaurantsHelper
   def restaurant_params
     params.require(:restaurant).permit(:name).merge(user: current_user)
   end
+
+  def star_rating(rating)
+    return rating unless rating.is_a?(Fixnum)
+    "\u2605" * rating + "\u2606" * (5 - rating)
+  end
 end
